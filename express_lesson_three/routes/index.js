4b8bbf28e6852cca86b3778bd4a15200963dff5a
Unknown <justinkjetton@gmail.com>
2018-05-18 17:31:20 -0500

    updated get method

--- express_lesson_three/routes/index.js
+++ express_lesson_three/routes/index.js
@@ -4,6 +4,10 @@ var users = require('../models/users');
 
 /* GET home page. */
 router.get('/person/:id', function(req, res, next) {
+  //get object that matches the id
+  let person = users.people.find(peep => {
+    return peep.id === parseInt(req.params.id);
+  });
   res.render('index', { title: 'Express' });
 });
 
